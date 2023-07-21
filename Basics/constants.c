10740deed4d1c2c97b310e204ace81d933136a35
xavierarce <119446640+xavierarce@users.noreply.github.com>
2023-07-21 11:01:59 -0500

    main

---
+++ Basics/constants.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+    //! Constant = fixed value that can not be altered
+  const float PI = 3.14159;
+
+  //* PI = 4.321; not possible cause its constant
+
+  printf("%.f",pi);
+
+    return 0;
+}
