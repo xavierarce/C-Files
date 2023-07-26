8360ede6ca02c9af947d8c59f0ffa6250b3a3cdf
xavierarce <119446640+xavierarce@users.noreply.github.com>
2023-07-26 00:22:41 -0500

    main

--- statements.c
+++ statements.c
@@ -4,22 +4,22 @@ int main(void){
 
   // //! IF && ELSE IF STATEMENT
 
-  // int age;
+  int age;
 
-  // printf("How old are you? ");
-  // scanf("%d",&age);
+  printf("How old are you? ");
+  scanf("%d",&age);
 
-  // if(age >= 18 && age <=99){
-  //   printf("You can drive!");
-  // }else if(age >= 100){
-  //   printf("You are super old!");
-  // }else if(age==0){
-  //   printf("You just borned");
-  // }else if(age <=0){
-  //   printf("You arent born yet!");
-  // }else{
-  //   printf("You are under age!");
-  // }
+  if(age >= 18 && age <=99){
+    printf("You can drive!");
+  }else if(age >= 100){
+    printf("You are super old!");
+  }else if(age==0){
+    printf("You just borned");
+  }else if(age <=0){
+    printf("You arent born yet!");
+  }else{
+    printf("You are under age!");
+  }
 
   //! SWITCH STATEMENT
   
