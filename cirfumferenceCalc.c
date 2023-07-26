9aabb1ec231078aafe4a852ff09dd2a48bac14ce
xavierarce <119446640+xavierarce@users.noreply.github.com>
2023-07-25 23:32:03 -0500

    main

--- cirfumferenceCalc.c
+++ cirfumferenceCalc.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+#include <math.h>
+
 
 int main(void){
+
+  //! Calculador de Cirfunferencia
+
   const double PI = 3.14159;
   double radius;
   double circumference;
   double area;
 
+  printf("Circunmference Calculator!\n")
   printf("Enter the radius of a circle: ");
   scanf("%lf",&radius);
 
@@ -15,6 +21,23 @@ int main(void){
   printf("Circumference %lf \n", circumference);
   printf("Area %lf \n", area);
 
+  
+  //! Calculador de Hypotenusa
+
+  double A;
+  double B;
+  double C;
+
+  printf("Hypothenus Calculator")
+
+  printf("Enter side A: ");
+  scanf("%lf", &A);
+  printf("Enter side B: ");
+  scanf("%lf", &B);
+
+  C = sqrt((A*A)+(B*B));
+
+  printf("Hypothenus of Triangle is = %lf",C);
 
 
   return 0;
