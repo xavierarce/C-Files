c0430fd2c8066fb9f806c9eb4cc351ac2f8b3471
xavierarce <119446640+xavierarce@users.noreply.github.com>
2023-07-26 17:15:25 -0500

    main

---
+++ loficalOperators.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void){
+
+  float temp = 100 ;  
+  bool sunny = 1; 
+
+  //! && Operator
+  if(temp >= 0 && temp <=35 || sunny==true){
+    printf("\nWeather is good");
+  }else{
+    (printf("\nCatastrofic man!"));
+  }
+
+  //! || Or Operator
+  if(temp >= 35 || temp <=0 ){
+    printf("\nWeather is bad");
+  }else{
+    (printf("\n Weather good!"));
+  }
+
+  //! ! not operator
+    if(!sunny){
+    printf("\nits cloudy outside");
+    }else{
+    printf("\nits not cloudy outside");
+    }
+
+
+  return 0;
+}
