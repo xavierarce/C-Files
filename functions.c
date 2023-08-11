6d797fd82e00e3a3bc68c8255d547cf5df638807
xavierarce <119446640+xavierarce@users.noreply.github.com>
2023-08-10 22:48:37 -0500

    main

---
+++ functions.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void salutation(int n){
+  for (int i = 0; i < n; i++) {
+    printf("\nHola nina");
+    printf("\nHola nifa");
+    printf("\nHola nife");
+    }
+}
+
+void greet(char z[], int y){
+  printf("Hello Im %s\nNice to meet you!\n",z);
+  printf("Im %d years old",y);
+}
+
+double square(double x){
+  return x*x;
+}
+double square1(double x){
+  double result = x*x;
+  printf("\nResult Square1 = %lf",result);
+}
+
+int main(void){
+
+  salutation(2);
+
+  char z[] = "Xavi";
+  int y = 21;
+  greet(z, y);
+  
+  greet("Xavi", 21);
+
+  double x = square(3.14);
+  printf("\nResult Square= %lf",x);
+
+  square1(3.2);
+
+  return 0;
+}
