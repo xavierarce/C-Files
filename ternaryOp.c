f452f415df8e6d8fdad85feb45b1c7be3261417e
xavierarce <119446640+xavierarce@users.noreply.github.com>
2023-08-13 23:24:08 -0500

    main

---
+++ ternaryOp.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+int findMax(int x, int y){
+  // if (x > y){
+  //   return x;
+  // }else{
+  //   return y;
+  // }
+  return (x>y)? x:  y;
+}
+
+int main(){
+
+  //! (Condition) ? value if true : value if false
+
+  int max= findMax(3,4);
+
+  printf("%d",max);  
+
+  return 0;
+}  
