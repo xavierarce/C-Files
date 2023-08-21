2cdf552c0be2c515d0d7018e8bbc8b4888d78ff6
xavierarce <119446640+xavierarce@users.noreply.github.com>
2023-08-20 23:43:40 -0500

    main

---
+++ loopsAll.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+
+//For Loops
+int main(){
+  
+  for(int i =1; i<=10 ; i++){
+    printf("%d",i);
+  }
+  
+  return 0;
+}
+
+
+// While Loops
+int main(){
+  char name[25];
+
+  printf("What's your name\n");
+  fgets(name,25,stdin);
+  name[strlen(name)-1]='\0';
+
+  while(strlen(name)==0){
+    printf("Need to enter a Name\n"); 
+    printf("What's your name\n");
+    fgets(name,25,stdin);
+    name[strlen(name)-1]='\0';
+  }
+
+  printf("Hello %s",name);
+
+  return 0;
+}
