c9b1edef8c10af2d05847ff279f6b6732adbce6e
xavierarce <119446640+xavierarce@users.noreply.github.com>
2023-08-22 23:47:20 -0500

    main

--- loopsAll.c
+++ loopsAll.c
@@ -36,6 +36,19 @@ int main(){
 int main(){
   char name[25];
 
+  printf("What's your name\n");
+  fgets(name,25,stdin);
+  name[strlen(name)-1]='\0';
+
+  while(strlen(name)==0){
+    printf("Need to enter your Name\n"); 
+    printf("What's your name\n");
+    fgets(name,25,stdin);
+    name[strlen(name)-1]='\0';
+  }
+
+  printf("Hello %s",name);
+
   return 0;
 }
 
